2a769bc63baac7ff8a6a024c847f2dd6bbb59647
AuroraMind <audiomindmail@gmail.com>
2021-07-11 20:30:36 +0200

    Validation of Optional Subscription E-Mail Field (Conditional Validation).

--- Angular-Forms/reactive-forms/src/app/app.component.ts
+++ Angular-Forms/reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { PasswordValidator } from './shared/password.validator';
 import { forbiddenNameValidator } from './shared/user-name.validator';
 
@@ -8,25 +8,45 @@ import { forbiddenNameValidator } from './shared/user-name.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {}
 
-  registrationForm = this.fb.group({
-    userName: ['', [Validators.required, Validators.minLength(2), forbiddenNameValidator(/password/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''],
-      state: [''],
-      postalCode: [''],
-    })
-    
-  },
-  {validator: PasswordValidator}
-  
-  );
+  ngOnInit(){
 
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(2), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: [''],
+      })
+      
+     },
+        {validator: PasswordValidator} 
+    );
+     this.registrationForm.get('subscribe')?.valueChanges
+      .subscribe( checkedvalue => 
+        {
+          const email = this.registrationForm.get('email');
+          if(checkedvalue)
+          {
+            email?.setValidators(Validators.required)
+          } else {
+            email?.clearValidators();
+          }
+          email?.updateValueAndValidity();
+        }
+        );
+  }
+
+  
   /*registrationForm = new FormGroup( 
     {
     userName: new FormControl( '', [Validators.required, Validators.minLength(2), forbiddenNameValidator]),
